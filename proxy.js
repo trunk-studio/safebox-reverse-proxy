8d16e3440e451350b750d371fd7fef7b2f3892b7
lyhcode <lyhcode@gmail.com>
2015-10-22 09:32:23 +0800

    test ok

--- proxy.js
+++ proxy.js
@@ -11,5 +11,3 @@ var proxy = new require('redbird')({
 // Subdomains, paths, everything just works as expected
 proxy.register("localhost/safebox", "http://localhost:9090/safebox");
 proxy.register("localhost/sbx_svr", "http://localhost:80/sbx_svr");
-
-/*proxy.register("localhost/sbx_svr", "http://localhost:8443/sbx_svr", {ssl: true});*/
