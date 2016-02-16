54f1aae2021fe416c00e485a49fc04ae6a1d7950
ironyun <ironyun@TrunkStudioMacMini.local>
2016-02-16 21:53:15 +0800

    update

--- proxy.js
+++ proxy.js
@@ -1,6 +1,6 @@
 var proxy = new require('redbird')({
     port: 8080,
-
+    xfwd: false,
     ssl: {
         port: 8443,
         key: "dev-key.pem",
@@ -10,4 +10,6 @@ var proxy = new require('redbird')({
 
 // Subdomains, paths, everything just works as expected
 proxy.register("localhost/safebox", "http://localhost:9090/safebox");
+proxy.register("192.168.168.66/safebox", "http://localhost:9090/safebox");
 proxy.register("localhost/sbx_svr", "http://localhost:80/sbx_svr");
+proxy.register("192.168.168.66/sbx_svr", "http://localhost:80/sbx_svr");
--- ssh-tunnel.sh
+++ ssh-tunnel.sh
@@ -1,3 +1,3 @@
-ssh -NfL 8088:localhost:8088 root@172.16.15.227
-ssh -NfL 5432:localhost:5432 root@172.16.15.227
-sudo ssh -NfL 80:localhost:80 root@172.16.15.227
+ssh -NfL 8088:localhost:8088 root@172.16.15.225
+ssh -NfL 5432:localhost:5432 root@172.16.15.225
+sudo ssh -NfL 80:localhost:80 root@172.16.15.225
